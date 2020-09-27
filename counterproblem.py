9057d56d9530afbae59e6b710d31c2a721050a99
samidha <tjerry0710@gmail.com>
2020-09-27 09:34:57 +0545

    counter problems

---
+++ counterproblem.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+X = int(input())
+shoes_sizes_list = list(map(int, input().split()))
+print(shoes_sizes_list)
+c = 0
+stock = Counter(shoes_sizes_list)
+print(stock)
+N = int(input())                            #no of customers
+for i in range(N):
+    shoe_size, money  = list(map(int, input().split()))
+    print(shoe_size, money)
+    if stock[shoe_size]:
+        c +=money
+        stock[shoe_size]-=1
+print(c)
+    
